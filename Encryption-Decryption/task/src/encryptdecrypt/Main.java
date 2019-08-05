40b9c24388f91ec5600332b0a13df2b1d9d6cd75
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-08-05 12:48:03 -0500

    now encrypts and decrypts messages. Uses every character and not only letters

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -6,9 +6,50 @@ public class Main {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        String operation = scanner.nextLine();
         String message = scanner.nextLine();
         String keyInput = scanner.nextLine();
         int key = Integer.parseInt(keyInput);
+
+        String output;
+
+        if (operation.equals("enc")) {
+            output = encryptMessage(message, key);
+        } else {
+            output = decryptMessage(message, key);
+        }
+
+
+        System.out.println(output);
+    }
+    public static String encryptMessage(String message, int key){
+
+        char[] messageArr = message.toCharArray();
+
+//        for (int i = 0; i < messageArr.length; i++) {
+//            char letter = messageArr[i];
+//            int asciiCode = letter;
+//
+//            if (asciiCode >= 97 && asciiCode <= 122) {
+//                int newAsciiCode = asciiCode + key;
+//                if (newAsciiCode > 122) {
+//                    newAsciiCode = 96 + (newAsciiCode % 122);
+//                }
+//                char newLetter = (char) newAsciiCode;
+//                messageArr[i] = newLetter;
+//            }
+//        }
+        for (int i = 0; i < messageArr.length; i++) {
+            char letter = messageArr[i];
+            int asciiCode = letter;
+            int newAsciiCode = asciiCode + key;
+            char newLetter = (char) newAsciiCode;
+            messageArr[i] = newLetter;
+        }
+        return new String(messageArr);
+    }
+
+    private static String decryptMessage(String message, int key) {
 
         char[] messageArr = message.toCharArray();
 
@@ -16,17 +57,11 @@ public class Main {
             char letter = messageArr[i];
             int asciiCode = letter;
 
-            if (asciiCode >= 97 && asciiCode <= 122) {
-                int newAsciiCode = asciiCode + key;
-                if (newAsciiCode > 122) {
-                    newAsciiCode = 96 + (newAsciiCode % 122);
-                }
-                char newLetter = (char) newAsciiCode;
-                messageArr[i] = newLetter;
+            int newAsciiCode = asciiCode - key;
+            char newLetter = (char) newAsciiCode;
+            messageArr[i] = newLetter;
             }
-        }
-        String encryptedMessage = new String(messageArr);
-        System.out.println(encryptedMessage);
+        return new String(messageArr);
     }
 }
 
--- Encryption-Decryption/task/test/EncryptDecryptTest.java
+++ Encryption-Decryption/task/test/EncryptDecryptTest.java
@@ -13,23 +13,35 @@ public class EncryptDecryptTest extends BaseStageTest {
     public List<PredefinedIOTestCase> generate() {
         return List.of(
             new PredefinedIOTestCase(
-                "welcome to hyperskill\n" +
-                "5",
-                "bjqhtrj yt mdujwxpnqq"),
-
+                "enc\n" +
+                    "Welcome to hyperskill!\n" +
+                    "5",
+                "\\jqhtrj%yt%m~ujwxpnqq&"),
             new PredefinedIOTestCase(
-                "treasure\n" +
-                    "10",
-                "dbokcebo"),
-
+                "enc\n" +
+                    "Hello\n" +
+                    "0",
+                "Hello"),
             new PredefinedIOTestCase(
-                "qdvdqvrxqwxrxwpvrxspvxiqgdiqarairpbiqqid\n" +
-                    "12",
-                "cphpchdjcijdjibhdjebhjucspucmdmudbnuccup"),
-
+                "enc\n" +
+                    "012345678\n" +
+                    "1",
+                "123456789"),
             new PredefinedIOTestCase(
-                "y\n" +
-                    "10",
-                "i"));
+                "dec\n" +
+                    "\\jqhtrj%yt%m~ujwxpnqq&\n" +
+                    "5",
+                "Welcome to hyperskill!"),
+            new PredefinedIOTestCase(
+                "dec\n" +
+                    "Hello\n" +
+                    "0",
+                "Hello"),
+            new PredefinedIOTestCase(
+                "dec\n" +
+                    "222233334444\n" +
+                    "1",
+                "111122223333")
+        );
     }
 }
